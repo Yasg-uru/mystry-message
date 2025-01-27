102f87874ecab15bab5e195f03465d9eb5b2eab7
yasg-uru <yashpawar12122004@gmail.com>
2025-01-27 16:14:50 +0530

    correct the error of the db connectivity

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -3,7 +3,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { NextResponse } from 'next/server';
 
 const openai = new OpenAI({
-  apiKey: "af2176b8192c4202934323b743112721",
+  apiKey: "sk-proj-OXZKAuDWggbDAmdelZtHT3BlbkFJZaW23nSfyeb9ahPky13w",
   // apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -11,11 +11,11 @@ export const runtime = 'edge';
 
 export async function POST(req: Request) {
   try {
-    const prompt ="Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'."
-      // "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
     const response = await openai.completions.create({
-      model: 'gpt-4',
+      model: 'gpt-3.5-turbo',
       // model: 'gpt-3.5-turbo-instruct',
       max_tokens: 400,
       stream: true,
